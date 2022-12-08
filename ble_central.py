f9144734297051201fc1a15b0f7b71911f136f54
michinchin <chinabigail08@yahoo.com>
2022-12-08 09:51:05 -0800

    update ble to use one characteristic

--- ble_central.py
+++ ble_central.py
@@ -8,23 +8,17 @@ from bleak import BleakScanner
 from bleak import discover
 
 
-on_value = bytearray([0x01])
-off_value = bytearray([0x00])
-# char_uuid = "f1b417b2-6e69-412a-8b13-ce91a48cdd90"
+# on_value = bytearray([0x01])
+# off_value = bytearray([0x00])
 
 curr_uuid = "d7a16eff-1ee7-4344-a3d2-a8203d97d75c"
-dest_uuid = "596a72cf-7acc-4181-a9d1-dbf30db2aa7b"
 
 async def setInput(client):
     command = input("Please enter move (i.e. 1 0):")
     cmds = command.split(' ')
-    # print(, int(cmds[1]))
-    # await client.write_gatt_char(curr_uuid, bytearray[b'' + int(cmds[0])], True)
-    
-    # await client.write_gatt_char(dest_uuid,  bytes(int(cmds[0])]), True) # decode with size of  
-    await client.write_gatt_char(dest_uuid,  bytes(cmds[0].encode('UTF-8')), True) # will need to decode
-    await client.write_gatt_char(dest_uuid,  bytes(cmds[1].encode('UTF-8')), True) # will need to decode
 
+    await client.write_gatt_char(curr_uuid,  bytearray([bytes(cmds[0].encode('UTF-8')), bytes(cmds[1].encode('UTF-8')) ]), True) 
+    
 async def run():
     print('Looking for nano ble peripheral')
 
